af2d8fd781f730c5ac0ca3a0475021340705a2ac
wuzelong <413652081@qq.com>
2021-07-01 16:31:39 +0800

    空气质量排行榜

--- app/src/main/java/com/thread0/weather/adapter/RvAdapterAirQuaH.kt
+++ app/src/main/java/com/thread0/weather/adapter/RvAdapterAirQuaH.kt
@@ -14,9 +14,9 @@ class RvAdapterAirQuaH : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private var timeList = mutableListOf<String>()
     private lateinit var mContext: Context
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder.itemView.tv_item_air_quality_aqis.text = numList[position]
-        holder.itemView.tv_item_air_quality_levels.text = levelList[position]
-        holder.itemView.tv_item_air_quality_time.text = timeList[position]
+        holder.itemView.tv_item_air_quality_rank_aqi.text = numList[position]
+        holder.itemView.tv_item_air_quality_rank_city.text = levelList[position]
+        holder.itemView.tv_item_air_quality_ranks.text = timeList[position]
     }
 
     fun setData(numsList: List<String>,levelsList: List<String>,timesList: List<String>) {
