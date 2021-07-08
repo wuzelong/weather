eb7abd0d609cdce199b10576dfb3b0e72c2b5728
wuzelong <413652081@qq.com>
2021-07-08 11:11:49 +0800

    修复：今日天气

--- app/src/main/java/com/thread0/weather/ui/activity/MainActivity.kt
+++ app/src/main/java/com/thread0/weather/ui/activity/MainActivity.kt
@@ -227,14 +227,14 @@ class MainActivity : AppCompatActivity() {
         launch{
             val result = weatherService.getDailyWeather(location=cityId,start="-1",days = "4")
             if(result != null){
-                val result0 = result.results[0].daily[0]
-                tv_temperture_range.text = result0.low+"°/"+result0.high+"°"
                 //昨今明后气温
+                val result0 = result.results[0].daily[0]
                 tv_yesterday_avg.text =((result0.low.toInt()+result0.high.toInt())/2).toString()+"°"
                 tv_yesterday_range.text = result0.low+"°/"+result0.high+"°"
                 val result1 = result.results[0].daily[1]
                 tv_today_avg.text =((result1.low.toInt()+result1.high.toInt())/2).toString()+"°"
                 tv_today_range.text = result1.low+"°/"+result1.high+"°"
+                tv_temperture_range.text = result1.low+"°/"+result1.high+"°"
                 val result2 = result.results[0].daily[2]
                 tv_tomorrow_avg.text = ((result2.low.toInt()+result2.high.toInt())/2).toString()+"°"
                 tv_tomorrow_range.text = result2.low+"°/"+result2.high+"°"
